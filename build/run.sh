391cc6c11b19ecc9fb288afede4c932dc0a23b49
Reuven Y <robi@post.jce.ac.il>
2021-12-23 14:43:48 +0200

    add --rm to docker run

--- build/run.sh
+++ build/run.sh
@@ -1,3 +1,3 @@
-GS_PORT=8080
+GS_PORT=8000
 
-docker run --name=geoserver_${GS_PORT} -p ${GS_PORT}:8080 -d -v $HOME/geoserver_data:/opt/geoserver/data_dir -e "HTTP_MAX_HEADER_SIZE=524288" -t thinkwhere/geoserver:2.20.1 /bin/sh -c conf/update_tomcat_settings.sh
+docker run --rm --name=geoserver_${GS_PORT} -p ${GS_PORT}:8080 -d -v $HOME/geoserver_data:/opt/geoserver/data_dir -e "HTTP_MAX_HEADER_SIZE=524288" -t thinkwhere/geoserver:2.20.1 /bin/sh -c conf/update_tomcat_settings.sh
